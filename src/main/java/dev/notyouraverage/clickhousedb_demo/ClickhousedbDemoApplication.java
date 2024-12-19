4981f8cdae6215009a037df690ec85d538859031
A Anand <a.anand.91119@gmail.com>
2024-12-19 17:37:32 +0530

    Initial Commit

---
+++ src/main/java/dev/notyouraverage/clickhousedb_demo/ClickhousedbDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.notyouraverage.clickhousedb_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickhousedbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickhousedbDemoApplication.class, args);
+	}
+
+}
